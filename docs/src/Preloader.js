5913d30880d5e968cbc322b8a2e46f47c6dd98ef
soyo <KatieIove@outlook.com>
2025-02-11 22:36:45 +0000

    fix a sb issue

--- docs/src/Preloader.js
+++ docs/src/Preloader.js
@@ -1,18 +1,15 @@
-export function preloader(p5){
-    export function preloader(p5) {
-        const isGithubPages = window.location.hostname.includes("github.io");
-        if(isGithubPages){
-            console.log("github page");
-        }
-
-        const basePath = isGithubPages ? "./assets/images/" : "../assets/images/";
-
-        let images = new Map();
-        images.set("Tree", p5.loadImage(basePath + "Tree.jpg"));
-        images.set("Bush", p5.loadImage(basePath + "Bush.jpg"));
-        images.set("Grass", p5.loadImage(basePath + "Grass.jpg"));
-
-        return images;
+export function preloader(p5) {
+    const isGithubPages = window.location.hostname.includes("github.io");
+    if (isGithubPages) {
+        console.log("github page");
     }
 
+    const basePath = isGithubPages ? "./assets/images/" : "../assets/images/";
+
+    let images = new Map();
+    images.set("Tree", p5.loadImage(basePath + "Tree.jpg"));
+    images.set("Bush", p5.loadImage(basePath + "Bush.jpg"));
+    images.set("Grass", p5.loadImage(basePath + "Grass.jpg"));
+
+    return images;
 }
