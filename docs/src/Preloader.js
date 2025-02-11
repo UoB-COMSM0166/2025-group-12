73b3cc0b75f97823c3f800cca47e5dcaf5b0e71b
saquantum <517393456@qq.com>
2025-02-11 22:06:21 +0000

    Update Preloader.js

--- docs/src/Preloader.js
+++ docs/src/Preloader.js
@@ -1,7 +1,7 @@
 export function preloader(p5){
     let images = new Map(); // <String name, Image img>
-    images.set("Tree", p5.loadImage("../assets/images/Tree.jpg"));
-    images.set("Bush", p5.loadImage("../assets/images/Bush.jpg"));
-    images.set("Grass", p5.loadImage("../assets/images/Grass.jpg"));
+    images.set("Tree", p5.loadImage("./docs/assets/images/Tree.jpg"));
+    images.set("Bush", p5.loadImage("./docs/assets/images/Bush.jpg"));
+    images.set("Grass", p5.loadImage("./docs/assets/images/Grass.jpg"));
     return images;
 }
