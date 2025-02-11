3bfb85d1bf8dbdcf605ae2ba73aac22ae0b23d9e
soyo <KatieIove@outlook.com>
2025-02-11 22:28:07 +0000

    tmp

--- docs/src/Preloader.js
+++ docs/src/Preloader.js
@@ -1,7 +1,14 @@
 export function preloader(p5){
+
+    const isGithubPages = window.location.hostname.includes("github.io");
+    if(isGithubPages){
+        console.log("github page");
+    }
+    const basePath = isGithubPages ? "./assets/images/" : "../assets/images/";
+
     let images = new Map(); // <String name, Image img>
-    images.set("Tree", p5.loadImage("../assets/images/Tree.jpg"));
-    images.set("Bush", p5.loadImage("../assets/images/Bush.jpg"));
-    images.set("Grass", p5.loadImage("../assets/images/Grass.jpg"));
+    images.set("Tree", p5.loadImage(basePath + "Tree.jpg"));
+    images.set("Bush", p5.loadImage(basePath + "Bush.jpg"));
+    images.set("Grass", p5.loadImage(basePath + "Grass.jpg"));
     return images;
 }
